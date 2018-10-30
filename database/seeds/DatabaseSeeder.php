e9854ce78c0df6bf84e0701b11386a86deba5fb5
Mohammad Muizz <madzabdurahman15@gmail.com>
2018-10-30 11:22:52 +0700

    Factory and Seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory('App\User',10)->create();
+        factory('App\Model\Category',5)->create();
+        factory('App\Model\Question',10)->create();
+        factory('App\Model\Reply',30)->create()->each(function($reply){
+            return $reply->like()->save(factory('App\Model\Like')->make());
+        });
     }
 }
