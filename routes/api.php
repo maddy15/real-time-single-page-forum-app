abcba4de10b23070ec78183118f11213529ddd15
Mohammad Muizz <madzabdurahman15@gmail.com>
2018-10-30 22:57:24 +0700

    Like and Dislikes for reply

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::apiResource('questions', 'QuestionController');
 Route::apiResource('categories', 'CategoryController');
 Route::apiResource('/questions/{question}/replies', 'ReplyController');
+
+Route::post('/like/{reply}','LikeController@likeIt');
+Route::delete('/like/{reply}','LikeController@unLikeIt');
+
