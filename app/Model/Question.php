2f4b31e96354e83d53bb33bf7deb73b4b7fbd8ca
Mohammad Muizz <madzabdurahman15@gmail.com>
2018-10-30 10:58:36 +0700

    Relationships

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function replies(){
+        return $this->hasMany('App\Model\Reply');
+    }
+
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function question(){
+        return $this->belongsTo('App\Model\Question');
+    }
+
+    public function like(){
+        return $this->hasMany('App\Model\Like');
+    }
 }
